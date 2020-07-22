dd51db90e33bcce9cc685a59cb5e7a59f2ab2f5a
halevy <hr@sem-wolf.org>
2020-07-22 13:52:07 +0300

    add function get_score

--- auto_complete.py
+++ auto_complete.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
 from auto_complete_data import AutoCompleteData
+from read_file import all_sentences
+import string
 
-all_sentences = [("hello big world", "file.txt"), ("hello world", "file2.txt")]
 subs = defaultdict(set)
 
 
@@ -23,9 +24,45 @@ def get_common_sentences(sentences_indexes):
         return list(sentences_indexes)
     return list(sentences_indexes)[:5]
 
-def get_score(sub_string,completed_sentence):
-    if sub_string in completed_sentence:
-        return len(sub_string)*2
+def get_score(sub_string):
+    basis_score = len(sub_string) * 2
+    score = defaultdict(set)
+    indexes = subs.get(sub_string)
+    if indexes:
+        score[basis_score] = indexes
+        if indexes >= 5:
+            return score
+
+    for i in range(len(sub_string)):
+        indexes = subs.get(sub_string.replace(sub_string[i], ""))
+        if i < 4:
+            score[basis_score - (10-i*2)] = indexes
+        else:
+            score[basis_score - 2] = indexes
+
+
+    all_alphabet = string.ascii_lowercase
+    for i in range(len(sub_string)):
+        for letter in all_alphabet:
+            indexes = subs.get(sub_string[:i]+letter+sub_string[i:])
+            if indexes:
+                if i < 4:
+                    score[basis_score - (10-i*2)] = indexes
+                else:
+                    score[basis_score - 2] = indexes
+
+    all_alphabet = string.ascii_lowercase
+    for i in range(len(sub_string)):
+        for letter in all_alphabet:
+            indexes = subs.get(sub_string.replace(sub_string[i],letter))
+            if indexes:
+                if i < 5:
+                    score[basis_score - (5-i)] = indexes
+                else:
+                    score[basis_score - 1] = indexes
+    return score
+
+
 
 def get_best_k_completions(sub_string):
 
@@ -35,7 +72,7 @@ def get_best_k_completions(sub_string):
         completed_sentence = all_sentences[index][0]
         source_text = all_sentences[index][1]
         offset = all_sentences[index][0].index(sub_string)
-        score = get_score(sub_string, completed_sentence)
+        score = get_score(sub_string)
         auto_complete_list.append(AutoCompleteData(completed_sentence, source_text, offset, score))
     return auto_complete_list
 
