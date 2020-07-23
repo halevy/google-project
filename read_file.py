9f197e139f79fda0c75a1398d61db056047ee228
halevy <hr@sem-wolf.org>
2020-07-23 10:06:42 +0300

    add read files

---
+++ read_file.py
@@ -0,0 +1,16 @@
+
+import os
+from auto_complete_data import AutoCompleteData
+all_sentences = []
+def read_txt_file():
+    for root, dirs, files in os.walk("./MyFiles/python-3.8.4-docs-text"):
+        for file in files:
+            if file.endswith(".txt"):
+                with open(os.path.join(root, file), encoding="utf8") as myfile:
+                    offset = 0
+                    sentences = myfile.readlines()
+                    for sentsnce in sentences:
+                        offset += 1
+                        all_sentences.append(AutoCompleteData(sentsnce.strip(), os.path.join(root, file), offset))
+
+read_txt_file()
